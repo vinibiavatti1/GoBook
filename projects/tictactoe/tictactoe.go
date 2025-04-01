4b2a6f2db9de5d0263e73b3dc9c4c3dd586eb38f
Vinicius Reif Biavatti <vinicius.biavatti@outsystems.com>
2025-04-01 22:54:27 +0100

    Fix

--- projects/tictactoe/tictactoe.go
+++ projects/tictactoe/tictactoe.go
@@ -27,15 +27,15 @@ const (
 )
 
 var PositionMap = map[string][2]int{
-	"a": [...]int{0, 0},
-	"b": [...]int{0, 1},
-	"c": [...]int{0, 2},
-	"d": [...]int{1, 0},
-	"e": [...]int{1, 1},
-	"f": [...]int{1, 2},
-	"g": [...]int{2, 0},
-	"h": [...]int{2, 1},
-	"i": [...]int{2, 2},
+	"1": [...]int{0, 0},
+	"2": [...]int{0, 1},
+	"3": [...]int{0, 2},
+	"4": [...]int{1, 0},
+	"5": [...]int{1, 1},
+	"6": [...]int{1, 2},
+	"7": [...]int{2, 0},
+	"8": [...]int{2, 1},
+	"9": [...]int{2, 2},
 }
 
 type Game struct {
@@ -53,9 +53,9 @@ func NewGame() *Game {
 
 func (g *Game) Reset() {
 	g.board = [...][Len]string{
-		[...]string{"a", "b", "c"},
-		[...]string{"d", "e", "f"},
-		[...]string{"g", "h", "i"},
+		[...]string{"1", "2", "3"},
+		[...]string{"4", "5", "6"},
+		[...]string{"7", "8", "9"},
 	}
 }
 
@@ -81,42 +81,50 @@ func (g *Game) SwapCurrentPlayer() {
 	}
 }
 
-func (g *Game) Validate() GameResult {
-	// Validate Rows
+func (g *Game) ValidateWinner() bool {
 	won := false
-	for i := range g.board {
-		won = g.board[i][0] == g.board[i][1] && g.board[i][1] == g.board[i][2]
-		if won {
-			return Won
-		}
-	}
-	// Validate Cols
-	for i := range g.board {
-		won = g.board[0][i] == g.board[1][i] && g.board[1][i] == g.board[2][i]
-		if won {
-			return Won
-		}
-	}
-	// Validate Diagonal
-	won = g.board[0][0] == g.board[1][1] && g.board[1][1] == g.board[2][2]
-	if won {
-		return Won
-	}
-	// Validate Inverse Diagonal
-	won = g.board[0][2] == g.board[1][1] && g.board[1][1] == g.board[2][0]
-	if won {
-		return Won
-	}
-	// Validate Tie
+	won = won || g.IsSamePlayerInPositions("1", "2", "3")
+	won = won || g.IsSamePlayerInPositions("4", "5", "6")
+	won = won || g.IsSamePlayerInPositions("7", "8", "9")
+	won = won || g.IsSamePlayerInPositions("1", "4", "7")
+	won = won || g.IsSamePlayerInPositions("2", "5", "8")
+	won = won || g.IsSamePlayerInPositions("3", "6", "9")
+	won = won || g.IsSamePlayerInPositions("1", "5", "9")
+	won = won || g.IsSamePlayerInPositions("3", "5", "7")
+	return won
+}
+
+func (g *Game) ValidateTie() bool {
 	for i := range g.board {
 		for j := range g.board {
 			pos := g.board[i][j]
 			if pos != Player1 && pos != Player2 {
-				return None
+				return false
 			}
 		}
 	}
-	return Tie
+	return true
+}
+
+func (g *Game) Validate() GameResult {
+	switch {
+	case g.ValidateWinner():
+		return Won
+	case g.ValidateTie():
+		return Tie
+	default:
+		return None
+	}
+}
+
+func (g *Game) IsSamePlayerInPositions(x, y, z string) bool {
+	xy1 := PositionMap[x]
+	xy2 := PositionMap[y]
+	xy3 := PositionMap[z]
+	x1, y1 := xy1[0], xy1[1]
+	x2, y2 := xy2[0], xy2[1]
+	x3, y3 := xy3[0], xy3[1]
+	return g.board[x1][y1] == g.board[x2][y2] && g.board[x2][y2] == g.board[x3][y3]
 }
 
 func (g *Game) PrintBoard() {
@@ -148,7 +156,7 @@ func (g *Game) Run() {
 			fmt.Println("Tie!!!")
 			running = false
 		case Won:
-			fmt.Printf("Player %s won!\n", g.currentPlayer)
+			fmt.Printf("Player %s Won!\n", g.currentPlayer)
 			running = false
 		}
 	}
